2eb70af53aed27d3bee253c51e5e925b30a54944
seunghwanandrew <108056027+seunghwanandrew@users.noreply.github.com>
2023-10-05 20:24:17 +0900

    Add files via upload

---
+++ Public/Characters/SlashCharacter.h
@@ -0,0 +1,53 @@
+
+#pragma once
+
+#include "InputActionValue.h"
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "SlashCharacter.generated.h"
+
+class USpringArmComponent;
+class UCameraComponent;
+class UInputMappingContext;
+class UInputAction;
+class UGroomComponent;
+
+UCLASS() 
+class SLASH_API ASlashCharacter : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	ASlashCharacter();
+	virtual void Tick(float DeltaTime) override;
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+protected:
+	virtual void BeginPlay() override;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "UserInput");
+	UInputMappingContext* CharacterMappingContext;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "UserInput");
+	UInputAction* MovementAction;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "UserInput");
+	UInputAction* LookAction;
+
+	void Movement(const FInputActionValue& Value);
+	void Look(const FInputActionValue& Value);
+
+private:
+	UPROPERTY(VisibleAnywhere)
+	USpringArmComponent* CameraBoom;
+
+	UPROPERTY(VisibleAnywhere)
+	UCameraComponent* ViewCamera;
+
+	UPROPERTY(VisibleAnywhere, Category = Hair)
+	UGroomComponent* Hair;
+
+	UPROPERTY(VisibleAnywhere, Category = Hair)
+	UGroomComponent* Eyebrows;
+};
